5e6844db3ae0d77a5b975226ee52ea567d5bf121
adamj <amrachek@outlook.com>
2018-09-26 17:27:17 -0600

    Added bonus features

--- lesson_2/rock_paper_scissors/rps_lizard_spock.rb
+++ lesson_2/rock_paper_scissors/rps_lizard_spock.rb
@@ -16,6 +16,16 @@ WINNING_MOVES = {
   'spock' => ['scissors', 'rock']
 }
 
+score = {
+  'player' => 0,
+  'computer' => 0
+}
+
+def reset_score(score_board)
+  # score_board.update(score_board) { |value| value = 0 }
+  score_board.each { |key, value| score_board[key] = 0 }
+end
+
 def prompt(message)
   puts "=> #{message}"
 end
@@ -34,6 +44,28 @@ def display_results(player, computer)
   end
 end
 
+def update_score(player, computer, score)
+  if win?(player, computer)
+    score['player'] += 1
+  else
+    score['computer'] += 1
+  end
+end
+
+def display_score(score)
+  puts "Player: #{score['player']}, Computer: #{score['computer']}"
+end
+
+def game_winner?(score)
+  if score['player'] == 5
+    'You won the game!'
+  elsif score['computer'] == 5
+    'Computer wins the game!'
+  else
+    false
+  end
+end
+
 prompt("----------------------------")
 prompt("Welcome to Rock, Paper, Scissors, Lizard, Spock!")
 prompt("----------------------------")
@@ -49,6 +81,7 @@ MSG
 
 loop do
   choice = ""
+
   loop do
     prompt(choice_prompt)
     choice = gets.chomp
@@ -65,10 +98,16 @@ loop do
   puts "You chose #{MOVES[choice]}; Computer chose #{computer_choice}."
 
   display_results(MOVES[choice], computer_choice)
+  update_score(MOVES[choice], computer_choice, score)
+  display_score(score)
 
-  prompt("Do you want to play again? (Enter 'y' to play again.)")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  if game_winner?(score)
+    prompt(game_winner?(score))
+    reset_score(score)
+    prompt("Do you want to play again? (Enter 'y' to play again.)")
+    answer = gets.chomp
+    break unless answer.downcase.start_with?('y')
+  end
 end
 
 prompt("Thank you for playing! Bye!")
