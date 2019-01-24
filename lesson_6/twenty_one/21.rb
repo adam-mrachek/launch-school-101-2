a7ab5f21c815a2c8e53e2eb0b585a369fbb32c78
adamj <amrachek@outlook.com>
2019-01-24 07:43:06 -0700

    Improved game loop. Added 5 round feature.

--- lesson_6/twenty_one/21.rb
+++ lesson_6/twenty_one/21.rb
@@ -14,6 +14,7 @@ DECK = [
   ['H', 'A'], ['D', 'A'], ['C', 'A'], ['S', 'A']
 ]
 
+WINNING_SCORE = 21
 DEALER_STAY = 17
 
 def prompt(msg)
@@ -35,7 +36,7 @@ def total(cards)
   end
 
   values.count('A').times do
-    sum -= 10 if sum > 21
+    sum -= 10 if sum > WINNING_SCORE
   end
 
   sum
@@ -45,8 +46,8 @@ def deal_card(hand, deck)
   hand << deck.pop
 end
 
-def busted?(cards)
-  total(cards) > 21
+def busted?(total)
+  total > WINNING_SCORE
 end
 
 def hand(cards)
@@ -61,8 +62,79 @@ def hand(cards)
   hand
 end
 
-def display_hand(cards)
-  prompt "You had #{hand(cards)} for a total of #{total(cards)}."
+def detect_result(player_total, dealer_total)
+  if player_total > WINNING_SCORE
+    :player_busted
+  elsif dealer_total > WINNING_SCORE
+    :dealer_busted
+  elsif dealer_total < player_total
+    :player
+  elsif dealer_total > player_total
+    :dealer
+  else
+    :draw
+  end
+end
+
+def display_result(player_total, dealer_total)
+  result = detect_result(player_total, dealer_total)
+
+  case result
+  when :player_busted
+    prompt "You busted! Dealer wins!"
+  when :dealer_busted
+    prompt "Dealer busted! You win!"
+  when :player
+    prompt "You win!"
+  when :dealer
+    prompt "Dealer wins!"
+  when :draw
+    prompt "It's a draw!"
+  end
+end
+
+def end_of_round_message(player_cards, dealer_cards)
+  puts ""
+  prompt "*****************"
+  prompt "Player's hand: #{hand(player_cards)} for a total of #{total(player_cards)}."
+  prompt "Dealer's hand: #{hand(dealer_cards)} for a total of #{total(dealer_cards)}"
+  prompt "*****************"
+  puts ""
+end
+
+def update_score(player_total, dealer_total, score)
+  result = detect_result(player_total, dealer_total)
+
+  case result
+  when :player
+    score[:player] += 1
+  when :dealer_busted
+    score[:player] += 1
+  when :dealer
+    score[:dealer] += 1
+  when :player_busted
+    score[:dealer] += 1
+  end
+  score
+end
+
+def winner?(score)
+  if score[:player] == 5
+    :player
+  elsif score [:dealer] == 5
+    :dealer
+  end
+end
+
+def display_winner(score)
+  winner = winner?(score)
+
+  case winner
+  when :player
+    prompt "Player wins the game!"
+  when :dealer
+    prompt "Dealer wins the game!"
+  end
 end
 
 def play_again?
@@ -81,95 +153,114 @@ prompt "----------------------"
 sleep 1.5
 
 loop do
+  score = {
+    player: 0,
+    dealer: 0
+  }
   loop do
-    system('clear') || system('cls')
-    game_deck = DECK.shuffle
-    player_cards = []
-    dealer_cards = []
-
-    puts "Dealing cards..."
-    sleep 1.5
-    system('clear') || system('cls')
-
-    2.times do
-      deal_card(player_cards, game_deck)
-      deal_card(dealer_cards, game_deck)
-    end
-
-    prompt "Dealer is showing a #{dealer_cards[0][1]}."
-    prompt "You have #{hand(player_cards)} for a total of #{total(player_cards)}"
-    sleep 1.5
-
-    # player turn
     loop do
-      player_choice = nil
-      loop do
-        prompt "Hit or stay? (h or s)"
-        player_choice = gets.chomp.downcase
-        break if ['h', 's'].include?(player_choice)
+      system('clear') || system('cls')
+      game_deck = DECK.shuffle
+      player_cards = []
+      dealer_cards = []
+      player_total = 0
+      dealer_total = 0
 
-        prompt "Invalid choice. Please choose to (h)it or (s)tay."
-      end
-
-      if player_choice == 'h'
-        deal_card(player_cards, game_deck)
-        prompt "You chose to hit!"
-        sleep 1.0
-        prompt "You have #{hand(player_cards)} for a total of #{total(player_cards)}"
-      end
-
-      break if busted?(player_cards) || player_choice == 's'
-    end
-
-    if busted?(player_cards)
-      prompt "You busted! Dealer wins."
-
-      break
-    else
-      prompt "You chose to stay."
-    end
-
-    # dealer turn
-    puts ""
-    puts "Dealer's turn..."
-    prompt "Dealer has #{hand(dealer_cards)} for a total of #{total(dealer_cards)}"
-    puts ""
-    sleep 1.0
-
-    loop do
-      break if total(dealer_cards) >= DEALER_STAY
-
-      prompt "Dealer hits!"
-      deal_card(dealer_cards, game_deck)
+      puts "Dealing cards..."
       sleep 1.5
-      prompt "Dealer now has #{hand(dealer_cards)} for a total of #{total(dealer_cards)}"
-    end
+      system('clear') || system('cls')
 
-    if busted?(dealer_cards)
-      prompt "Dealer busted. You win!"
+      2.times do
+        deal_card(player_cards, game_deck)
+        deal_card(dealer_cards, game_deck)
+      end
+
+      player_total = total(player_cards)
+      dealer_total = total(dealer_cards)
+
+      prompt "Dealer is showing a #{dealer_cards[0][1]}."
+      prompt "You have #{hand(player_cards)} for a total of #{player_total}"
+      sleep 1.5
+
+      # player turn
+      loop do
+        player_choice = nil
+        loop do
+          prompt "Hit or stay? (h or s)"
+          player_choice = gets.chomp.downcase
+          break if ['h', 's'].include?(player_choice)
+
+          prompt "Invalid choice. Please choose to (h)it or (s)tay."
+        end
+
+        if player_choice == 'h'
+          deal_card(player_cards, game_deck)
+          prompt "You chose to hit!"
+          player_total = total(player_cards)
+          sleep 1.0
+          prompt "You have #{hand(player_cards)} for a total of #{player_total}"
+        end
+
+        break if busted?(player_total) || player_choice == 's'
+      end
+
+      if busted?(player_total)
+        display_result(player_total, dealer_total)
+        end_of_round_message(player_cards, dealer_cards)
+        score = update_score(player_total, dealer_total, score)
+
+        break
+      else
+        prompt "You chose to stay."
+      end
+
+      # dealer turn
+      puts ""
+      puts "Dealer's turn..."
+      prompt "Dealer has #{hand(dealer_cards)} for a total of #{dealer_total}"
+      puts ""
+      sleep 1.0
+
+      loop do
+        break if dealer_total >= DEALER_STAY
+
+        prompt "Dealer hits!"
+        deal_card(dealer_cards, game_deck)
+        dealer_total = total(dealer_cards)
+        sleep 1.5
+        prompt "Dealer now has #{hand(dealer_cards)} for a total of #{dealer_total}"
+      end
+
+      if busted?(dealer_total)
+        display_result(player_total, dealer_total)
+        end_of_round_message(player_cards, dealer_cards)
+        score = update_score(player_total, dealer_total, score)
+
+        break
+      else
+        prompt "Dealer stays."
+      end
+
+      sleep 1.5
+
+      end_of_round_message(player_cards, dealer_cards)
+
+      display_result(player_total, dealer_total)
+
+      score = update_score(player_total, dealer_total, score)
+
       break
-    else
-      prompt "Dealer stays."
     end
 
-    sleep 1.5
+    prompt "Player's Score: #{score[:player]}"
+    prompt "Dealer's Score: #{score[:dealer]}"
 
-    puts ""
-    prompt "*****************"
-    prompt "You have #{hand(player_cards)} for a total of #{total(player_cards)}."
-    prompt "Dealer has #{hand(dealer_cards)} for a total of #{total(dealer_cards)}"
-    prompt "*****************"
-    puts ""
+    display_winner(score) if winner?(score)
 
-    if total(dealer_cards) < total(player_cards)
-      prompt "You win!"
-    elsif total(dealer_cards) > total(player_cards)
-      prompt "Dealer wins!"
-    else
-      prompt "It's a draw!"
-    end
+    break if winner?(score)
 
-    break
+    prompt "Hit enter to play next round."
+    gets.chomp
   end
 
   sleep 1.5
